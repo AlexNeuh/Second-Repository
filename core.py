5810d23eb8628e68b42722aa2e482fa8096138e6
Alex <neuhofer.alexander@gmx.at>
2020-11-06 10:45:57 +0100

    Add core and test_core file
    Core includes function foo
    Test_core includes test for function foo

---
+++ core.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 6, 2020
+
+@author: Alexander Neuhofer
+
+
+"""
+
+def foo(x):
+    """ Simple function to explain usage of test file. """
+    
+    # Returns input + 1
+    return + 1
