ff783b3195027a7cccc190dbe8145017bbafedff
AlexNeuh <44610484+AlexNeuh@users.noreply.github.com>
2020-11-10 20:17:55 +0100

    Create test_Aufg1a_ANeuhofer.py

---
+++ test_Aufg1a_ANeuhofer.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10, 2020
+@author: Alexander Neuhofer
+"""
+from Aufg1a_ANeuhofer import is_prime
+
+
+def test_isPrime():
+    """ Simple function to explain usage of test file. 
+	- Each function should be checked with a test! """
+    
+    # assert = "behaupte, dass ..."
+    assert is_prime(3) == "Die Zahl  3  ist wirklich eine Primzahl!"
